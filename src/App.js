4524fd1fa4ab0b697fabeabd0b362fc011bb5fa6
Evandro Carreira <evandro.carreira@gmail.com>
2019-09-28 15:55:57 -0300

    Lendo arquivos e extraindo informações

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MaterialTable from 'material-table';
+import { forwardRef } from 'react';
 
-function App() {
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import CloudDownload from '@material-ui/icons/CloudDownload';
+import { dados } from './data'
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
+
+export default function App() {
+
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Número do edital/licitação', field: 'num_edital' },
+      { title: 'Orgão', field: 'orgao' },
+      { title: 'Modalidade', field: 'modalidade' }, 
+      {
+        title: 'Status',
+        field: 'status',
+        lookup: { 1: 'Homologado', 2: 'Pós-sessão' },
+      },
+      { title: 'Início da sessão', field: 'inicio' },
+      { title: 'Objeto completo', field: 'objeto_completo' },
+      { title: 'Valor estimado', field: 'valor_estimado' },
+      { title: 'Edital completo', field: 'link_edital' },
+      { title: 'Quem validou', field: 'quem_validou' },
+    ],
+
+    data: dados,
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MaterialTable
+      icons={tableIcons}
+      title="Controle de editais"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.push(newData);
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data[data.indexOf(oldData)] = newData;
+              setState({ ...state, data });
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.splice(data.indexOf(oldData), 1);
+              setState({ ...state, data });
+            }, 600);
+          }),
+      }}
+    />
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import CloudDownload from '@material-ui/icons/CloudDownload';
+
+export const dados = [
+    {
+        "num_edital": "252/SMA/DSLC/2017",
+        "orgao": "Secretaria Municipal da Administração",
+        "modalidade": "PREGÃO ELETRÔNICO",
+        "exclusivo_micro": false,
+        "inicio": "04 de outubro de 2017, às 10:30h",
+        "link_edital": <CloudDownload/>,
+        "status": 1,
+        "quem_validou": "Evandro",
+        "valor_estimado": "R$ 1.308.234,78",
+        "objeto_completo": "aquisição de rolo de obreia 25mm, alvo padrão silhueta, algemas, kit limpeza para pistola e espingarda."
+    },
+    {
+        "num_edital": "252/SMA/DSLC/2017",
+        "orgao": "Secretaria Municipal da Administração",
+        "modalidade": "PREGÃO ELETRÔNICO",
+        "exclusivo_micro": false,
+        "link_edital": <CloudDownload/>,
+        "status": 2,
+        "quem_validou": "Fabiana",
+        "inicio": "04 de outubro de 2017, às 10:30h",
+        "valor_estimado": "6.1. As despesas decorrentes do presente pregão correrão à conta dos recursos previstos no orçamento da Secretaria Municipal da Segurança Pública, na dotação orçamentária: 37.04.2.658, e pelo elemento de despesa: 3.3.90.30.00.00 – Fonte de Recurso: 0031. ",
+        "objeto_completo": "O presente pregão eletrônico tem como objeto a aquisição de rolo de obreia 25mm, alvo padrão silhueta, algemas, kit limpeza para pistola e espingarda, de acordo com a disponibilidade dos produtos e, ainda, conforme especificações, quantitativos e condições estabelecidas no Anexo I e nas condições previstas neste Edital."
+    },
+    {
+        "num_edital": "285/SMA/DSLC/2017",
+        "orgao": "Secretaria Municipal da Administração",
+        "modalidade": "PREGÃO ELETRÔNICO",
+        "exclusivo_micro": false,
+        "status": 2,
+        "link_edital": <CloudDownload/>,
+        "quem_validou": "Joyce",
+        "inicio": "01 de novembro de 2017, às 09:30h",
+        "valor_estimado": "6.1. As despesas decorrentes do presente pregão correrão à conta dos recursos previstos no orçamento da Secretaria Municipal de Infraestrutura, na dotação orçamentária: 2380, e pelo elemento de despesa: 3.3.90.30.00 - 0080. ",
+        "objeto_completo": "O presente pregão eletrônico tem como objeto o Registro de Preços para a contratação de empresa especializada para o fornecimento de combustível (Diesel S-10), para o abastecimento de veículos da frota Municipal de Florianópolis (Secretaria Municipal de Infraestrutura), de acordo com a disponibilidade dos produtos e, ainda, conforme especificações, quantitativos e condições estabelecidas no Anexo I e nas condições previstas neste Edital."
+    },
+    {
+        "num_edital": "900/SMA/DSLC/2018",
+        "orgao": "Secretaria Municipal da Administração",
+        "modalidade": "PREGÃO ELETRÔNICO",
+        "exclusivo_micro": false,
+        "link_edital": <CloudDownload/>,
+        "quem_validou": "Fabiana",
+        "inicio": "12 de fevereiro de 2019, às 14h",
+        "valor_estimado": "6.1. As despesas decorrentes do presente pregão correrão à conta dos recursos previstos no orçamento do Fundo Municipal de Saúde, no Projeto/Atividade: 4.177; pelo elemento de despesa: 4.4.90.52.00.00.00.00 – Fonte de Recurso: 2115. ",
+        "objeto_completo": "O presente pregão eletrônico tem como objeto a contratação de empresa para fornecimento de veículos novo Tipo furgão adaptado para Ambulância de Suporte Básico Tipo B, conforme portaria 2048/2002 do Ministério da Saúde, conforme especificações, quantitativos e condições estabelecidas no Anexo I e nas condições previstas neste Edital."
+    },
+    {
+        "num_edital": "900/SMA/DSLC/2018",
+        "orgao": "Secretaria Municipal da Administração",
+        "modalidade": "PREGÃO ELETRÔNICO",
+        "exclusivo_micro": false,
+        "link_edital": <CloudDownload/>,
+        "quem_validou": "Fabiana",
+        "inicio": "28 de janeiro de 2019, às 16h",
+        "valor_estimado": "6.1. As despesas decorrentes do presente pregão correrão à conta dos recursos previstos no orçamento do Fundo Municipal de Saúde, no Projeto/Atividade: 4.177; pelo elemento de despesa: 4.4.90.52.00.00.00.00 – Fonte de Recurso: 2115. ",
+        "objeto_completo": "O presente pregão eletrônico tem como objeto a contratação de empresa para fornecimento de veículos novo Tipo furgão adaptado para Ambulância de Suporte Básico Tipo B, conforme portaria 2048/2002 do Ministério da Saúde, conforme especificações, quantitativos e condições estabelecidas no Anexo I e nas condições previstas neste Edital."
+    },
+    {
+        "num_edital": "889/SMA/DSLC/2018",
+        "orgao": "Secretaria Municipal da Administração",
+        "modalidade": "PREGÃO ELETRÔNICO",
+        "exclusivo_micro": true,
+        "link_edital": <CloudDownload/>,
+        "quem_validou": "Fabiana",
+        "inicio": "24 de janeiro de 2019, às 13h30min",
+        "valor_estimado": "6.1. As despesas decorrentes do presente pregão correrão à conta dos recursos previstos no orçamento da Autarquia de Melhoramentos da Capital - COMCAP, pelo elemento de despesa: 3.3.90.30, Fonte de Recurso: 80. ",
+        "objeto_completo": "O presente pregão eletrônico tem como objeto o Registro de Preços para contratação de empresa especializada para o fornecimento de materiais de consumo para a borracharia (fornecimento de cola pra vulcanização, remendo a quente e a frio, válvula, vaselina solida, acoplador (bico)), visando atendimento das demandas da Autarquia de Melhoramentos da Capital – COMCAP, de acordo com a disponibilidade dos produtos e, ainda, conforme especificações, quantitativos e condições estabelecidas nos Anexos e nas condições previstas neste Edital."
+    },
+    {
+        "num_edital": "888/SMA/DSLC/2018",
+        "orgao": "Secretaria Municipal da Administração",
+        "modalidade": "PREGÃO ELETRÔNICO",
+        "exclusivo_micro": true,
+        "link_edital": <CloudDownload/>,
+        "quem_validou": "Fabiana",
+        "inicio": "24 de janeiro de 2019, às 15h30min",
+        "valor_estimado": "6.1. As despesas decorrentes do presente pregão correrão à conta dos recursos previstos no orçamento da Autarquia de Melhoramentos da Capital - COMCAP, pelo elemento de despesa: 3.3.90.30, Fonte de Recurso: 80. ",
+        "objeto_completo": "O presente pregão eletrônico tem como objeto o Registro de Preços para contratação de empresa especializada para o fornecimento de tintas Automotivas e complementos, destinados à reposição de estoque do almoxarifado da COMCAP, de acordo com a disponibilidade dos produtos e, ainda, conforme especificações, quantitativos e condições estabelecidas nos Anexos e nas condições previstas neste Edital."
+    },
+    {
+        "num_edital": "884/SMA/DSLC/2018",
+        "orgao": "Secretaria Municipal da Administração",
+        "modalidade": "PREGÃO ELETRÔNICO",
+        "exclusivo_micro": true,
+        "link_edital": <CloudDownload/>,
+        "quem_validou": "Fabiana",
+        "inicio": "24 de janeiro de 2019, às 17h30min",
+        "valor_estimado": "6.1. As despesas decorrentes do presente pregão correrão à conta dos recursos previstos no orçamento da Autarquia de Melhoramentos da Capital - COMCAP, pelo elemento de despesa: 3.3.90.30, Fonte de Recurso: 80. ",
+        "objeto_completo": "O presente pregão eletrônico tem como objeto o Registro de Preços para contratação de empresa especializada para o fornecimento de peças de REPOSIÇÃO COM AS MESMAS ESPECIFICAÇÕES TÉCNICAS E CARACTERISTICAS DE QUALIDADE DE PEÇA DE PRODUÇÃO ORIGINAL (ABNT NBR 15296), para veículos das marcas FIAT, Massey Ferguson e New Holland pertencentes à Autarquia de Melhoramentos da Capital – COMCAP, de acordo com a disponibilidade dos produtos e, ainda, conforme especificações, quantitativos e condições estabelecidas nos Anexos e nas condições previstas neste Edital."
+    }
+
+]
